44e1dc91f8032a8555d26a76f7723a207587663a
Dmitry Kulinchenko <dima.kul22@mail.ru>
2023-06-04 21:10:54 +0300

    fix tests for schedule module

--- api/tests/test_schedule_api.py
+++ api/tests/test_schedule_api.py
@@ -21,26 +21,26 @@ class ScheduleApiTest(APITestCase):
         )
         self.assertEqual(self.response.status_code, status.HTTP_200_OK)
 
-        # courseGroup_3_5 = CourseGroup.objects.create(course_number=3, group_number='5', higher_education_level='b')
-        # Schedule.objects.create(schedule_file='schedules/3_5.2_b.json', course_group_id=courseGroup_3_5.pk)
-        # self.student = User.objects.create_user(username='andrew', email='maloy@gmail.com',
-        #                                         password='pla232piSR', first_name='Андрей',
-        #                                         second_name='Иванков', patronymic='Валерьевич', phone='+79009284593')
-        # Student.objects.create(year_of_enrollment='2', record_book_number='1629071',
-        #                        course_group_id=courseGroup_3_5.pk, user_id=self.student.pk)
-        # self.url = '/api/auth/jwt/create/'
-        # self.resp = self.client.post(
-        #     self.url,
-        #     {
-        #         'username': 'andrew',
-        #         'password': 'pla232piSR'
-        #     },
-        #     format='json'
-        # )
-        # self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+        courseGroup_3_5 = CourseGroup.objects.create(course_number=3, group_number='5', higher_education_level='b')
+        Schedule.objects.create(schedule_file='schedules/3_5.2_b.json', course_group_id=courseGroup_3_5.pk)
+        self.student = User.objects.create_user(username='andrew', email='maloy@gmail.com',
+                                                password='pla232piSR', first_name='Андрей',
+                                                second_name='Иванков', patronymic='Валерьевич', phone='+79009284593')
+        Student.objects.create(year_of_enrollment='2', record_book_number='1629071',
+                               course_group_id=courseGroup_3_5.pk, user_id=self.student.pk)
+        self.url = '/api/auth/jwt/create/'
+        self.resp = self.client.post(
+            self.url,
+            {
+                'username': 'andrew',
+                'password': 'pla232piSR'
+            },
+            format='json'
+        )
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
 
     def test_get_lessons_list_for_professor(self):
-        endpoint = '/api/schedule/1/?w=n'
+        endpoint = '/api/schedule/1/'
         response = self.client.get(endpoint)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -49,17 +49,14 @@ class ScheduleApiTest(APITestCase):
         response = self.client.get(endpoint)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(type(response.data), dict)
-
-
-
         data = response.data
         schedule = {
-            'monday': data['numerator']['monday'],
-            'tuesday': data['numerator']['tuesday'],
-            'wednesday': data['numerator']['wednesday'],
-            'thursday': data['numerator']['thursday'],
-            'friday': data['numerator']['friday'],
-            'saturday': data['numerator']['saturday']
+            'monday': data['numerator']['monday'] + data['denominator']['monday'],
+            'tuesday': data['numerator']['tuesday'] + data['denominator']['tuesday'],
+            'wednesday': data['numerator']['wednesday'] + data['denominator']['wednesday'],
+            'thursday': data['numerator']['thursday'] + data['denominator']['thursday'],
+            'friday': data['numerator']['friday'] + data['denominator']['friday'],
+            'saturday': data['numerator']['saturday'] + data['denominator']['saturday']
         }
         lessonsCountPerWeek = 0
         days = schedule.keys()
@@ -68,9 +65,9 @@ class ScheduleApiTest(APITestCase):
             for i in range(lessonsCountPerDay):
                 if len(schedule[key][i]) != 0:
                     lessonsCountPerWeek += 1
-        self.assertEqual(lessonsCountPerWeek, 6)
+        self.assertEqual(lessonsCountPerWeek, 14)
 
-    def test_get_lessons_list_per_week_for_professor(self):
+    def test_get_lessons_list_per_denominator_for_professor(self):
         token = self.response.data['access']
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
@@ -86,16 +83,24 @@ class ScheduleApiTest(APITestCase):
                     lessonsCountPerDenominator += 1
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(type(schedule), dict)
-        self.assertEqual(lessonsCountPerDenominator, 7)
+        self.assertEqual(lessonsCountPerDenominator, 9)
 
     def test_get_lessons_list_for_student(self):
-        token = self.response.data['access']
+        token = self.resp.data['access']
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
-        # self.client.login(username=self.student.username, password='pla232piSR')
         endpoint = '/api/schedule/2/?week=a'
         response = self.client.get(endpoint)
-        schedule = response.data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.data
+        schedule = {
+            'monday': data['numerator']['monday'] + data['denominator']['monday'],
+            'tuesday': data['numerator']['tuesday'] + data['denominator']['tuesday'],
+            'wednesday': data['numerator']['wednesday'] + data['denominator']['wednesday'],
+            'thursday': data['numerator']['thursday'] + data['denominator']['thursday'],
+            'friday': data['numerator']['friday'] + data['denominator']['friday'],
+            'saturday': data['numerator']['saturday'] + data['denominator']['saturday']
+        }
+        print(schedule)
         days = schedule.keys()
         lessonsCountPerTwoWeek = 0
         for key in days:
@@ -103,6 +108,6 @@ class ScheduleApiTest(APITestCase):
             for i in range(lessonsCountPerDay):
                 if len(schedule[key][i]) != 0:
                     lessonsCountPerTwoWeek += 1
-        self.assertEqual(lessonsCountPerTwoWeek, 7)
+        self.assertEqual(lessonsCountPerTwoWeek, 13)
 
     
